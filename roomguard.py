31480420a59b2f51e03c18c291fa3bac137ee577
PJ Evans <pj@mrpjevans.com>
2020-01-03 16:39:23 +0000

    Added notification support

--- roomguard.py
+++ roomguard.py
@@ -1,7 +1,16 @@
 import time
+import os.path
 import automationhat
+import apprise
 
 alarm_sounding = False
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+apobj = apprise.Apprise()
+config = apprise.AppriseConfig()
+config.add(script_dir + '/.apprise')
+apobj.add(config)
 
 print("Arming in 10 seconds")
 time.sleep(10)
@@ -15,13 +24,23 @@ while True:
     # If new motion detected sound the alarm
     if motion_detected and alarm_sounding != True:
         print("Motion detected, loud noise time")
-        automationhat.relay.one.on()
+        #automationhat.relay.one.on()
         alarm_sounding = True
+
+        apobj.notify(
+            body='Motion detected! Yikes!',
+            title='RoomGuard',
+        )
 
     # If motion no longer detected stop the alarm
     if motion_detected != True and alarm_sounding:
         print("It's all gone quiet, switching off")
         automationhat.relay.one.off()
         alarm_sounding = False
+
+        apobj.notify(
+            body='No more motion detected',
+            title='RoomGuard',
+        )
 
     time.sleep(0.5)
